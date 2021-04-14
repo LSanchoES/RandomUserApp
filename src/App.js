2c8993f34e28731a5316f8c0fb1c7f8b3ace20d5
lsancho <lsanchoaragon@outlook.com>
2021-04-14 10:03:32 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { UserCard } from "./Components/UserCard";
+import { useFetch } from "./Hooks/useFetch";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+	
+    const [state] = useState(1)
+
+
+  
+	const url = `https://reqres.in/api/users/${state}`;
+
+	const { loading } = useFetch(url);
+
+
+	return (
+		<div className="container p-1 p-lg-4 d-flex justify-content-center row">
+			{loading ? (
+				<div className=" alert-info">Loading....</div>
+			)
+			 :
+			
+			(
+				<>
+
+				<UserCard  />
+				<UserCard  />
+				<UserCard  />
+				
+				</>
+			)}
+		</div>
+	);
+};
 
 export default App;
